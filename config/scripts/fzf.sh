340b2b44eb7d84f16820c5a579bfe22668baa59c
Brighton Cox <brightonlcox@gmail.com>
2026-03-29 23:55:36 -0600

    updated fzf shell to have preview command

--- config/scripts/fzf.sh
+++ config/scripts/fzf.sh
@@ -2,7 +2,20 @@
 
 SESSION="$1"
 
-FILE=$(rg --files --ignore | fzf)
+FILE=$(rg --files --ignore | SHELL=bash fzf \
+  --preview='
+    file={1}
+    if command -v bat &>/dev/null; then
+      bat --color=always --style=numbers,header \
+          "$file"
+    else
+      nl -ba "$file" | sed -n "$((line-10)),$((line+10))p"
+    fi
+  ' \
+  --preview-window='right:60%:~3:+{2}-5' \
+  --bind='ctrl-/:toggle-preview' \
+  --prompt='Go to > ' \
+  --header='CTRL-/ to toggle preview')
 
 if [[ -n "$FILE" ]]; then
     booster exec -s "$SESSION" "o $FILE"
